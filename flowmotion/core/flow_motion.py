d7dc8e4a6df88a53ec18f6120c44d42073844d89
MohammadSenanAli <mdsenan@gmail.com>
2025-07-26 16:26:45 +0530

    refactoring and mute logic update

--- flowmotion/core/flow_motion.py
+++ flowmotion/core/flow_motion.py
@@ -1,30 +1,27 @@
-# flowmotion/core/flow_motion.py
 from datetime import datetime
+from enum import Enum
 
 
 class FlowMotion:
+    """
+    Base class for flowmotion objects providing action types and logging.
+    """
+
+    class FlowAction(Enum):
+        ADD = "ADD"
+        PLAY = "PLAY"
+        SKIP = "SKIP"
+        REMOVE = "REMOVE"
+
     _log_file_path = "flowmotion.log"
-
-    def __init__(self):
-        self.mute_animation = False
-
-    def is_mute(self):
-        return self.mute_animation
-
-    def unmute(self):
-        self.mute_animation = True
-        return
-
-    @property
-    def mute(self):
-        self.mute_animation = True
-        return self
 
     @classmethod
     def log(cls, message: str):
         """
-        Log a message with timestamp and class name to a file.
-        Usage: self.log("Some message")
+        Log a message with timestamp and class name to the flowmotion log file.
+
+        Args:
+            message (str): The message to log.
         """
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         log_line = f"[{timestamp}] [{cls.__name__}] {message}"
