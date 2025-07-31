b484912b09d2a65341755734bc5f71995196ce8b
MohammadSenanAli <mdsenan@gmail.com>
2025-07-31 23:03:15 +0530

    chore(release): v0.2.5

--- flowmotion/__init__.py
+++ flowmotion/__init__.py
@@ -4,7 +4,7 @@ FlowMotion Library
 A lightweight animation framework built on top of Manim for visualizing algorithms and data flow.
 """
 
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 
 import logging
 from rich.logging import RichHandler
